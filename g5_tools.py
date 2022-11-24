dc9414e84d147273d70a196721c480426f33f94a
Anon <anon@example.com>
2022-11-24 20:05:59 +0100

    Minor improvements

--- g5_tools.py
+++ g5_tools.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python3
 
-import fire
 from pathlib import Path
-import nltk
+import pathlib
+
+import fire
 import sarge
 
 from build_coha_corpus import build_train_test, build_data_sets
 
 DEFAULT_DATA_ROOT_DIR='data/'
+
+def setup_punkt():
+    '''Punkt tokenizer is used by the COHA preprocessing code (build_corpus...)'''
+    import nltk
+    nltk.download('punkt')
 
 def _freeze_hashdeep_rel(data_dirpath=DEFAULT_DATA_ROOT_DIR, hashdeep_outfile_name='data.hashdeep.rel.txt'):
     command = ' hashdeep -revv -l "{}" > "{}" '
@@ -27,34 +33,34 @@ def freeze_hashdeep(data_dirpath=DEFAULT_DATA_ROOT_DIR):
     _freeze_hashdeep_bare(data_dirpath)
 
 class coha(object):
+
     @staticmethod
     def download_sample():
         '''example CLI invocation: python g5_tools.py coha download_sample '''
         url = 'https://www.corpusdata.org/coha/samples/text.zip'
-        command = sarge.shell_format('wget {}', url)
-        sarge.run(command)
+        out_file_path = Path(Path(url).name)
+        if out_file_path.is_file():
+            print('Not downloading because there is already a file at: {} ({})'.format(out_file_path, out_file_path.absolute()))
+        else:
+            command = sarge.shell_format('wget {}', url)
+            sarge.run(command)
 
     @staticmethod
-    def build_corpus(dirpaths_for_input_slices:list, data_root_dir=DEFAULT_DATA_ROOT_DIR):
+    def build_corpus(*dirpaths_for_input_slices, data_root_dir=DEFAULT_DATA_ROOT_DIR):
         '''Wrapper alternative to simplify the argparse version in the original build_coha_corpus.py.
 
         Invoke like so:
-        python g5_tools.py coha build_corpus '["data/coha/coha_1880s","data/coha/coha_1900s"]'
-
-        or:
-        python g5_tools.py coha build_corpus '["data/coha/coha_1883","data/coha/coha_1908"]'
-
-        (mind the extra pair of quotes, which may be necessary according to your version of python, fire, shell, etc.)
+        python g5_tools.py coha build_corpus "data/coha/coha_1883/"  "data/coha/coha_1908/"
         '''
 
         if __name__ == '__main__':
-            nltk.download('punkt')
+            setup_punkt()
 
         data_root_dir = Path(data_root_dir)
-        output_dir = data_root_dir / Path('outputs') # TBD: make this work if outputs dir does not exist yet
+        output_dir = data_root_dir / Path('outputs')
 
         input_folders = [ Path(dirpath) for dirpath in dirpaths_for_input_slices ]
-        output_files = [ output_dir / Path(str(Path(dirpath).stem) + '.txt') for dirpath in dirpaths_for_input_slices ]
+        output_files = [ output_dir / Path(str(Path(dirpath).name) + '.txt') for dirpath in dirpaths_for_input_slices ]
 
         lm_output_train = output_dir / Path('coha.train.txt')
         lm_output_test = output_dir / Path('coha.test.txt')
@@ -64,6 +70,7 @@ class coha(object):
             print(e)
 
         print('\nOk, making changes to filesystem...')
+        output_dir.mkdir(exist_ok=True) # create outputs dir if it does not exist yet
         build_train_test(input_folders, lm_output_train, lm_output_test)
         build_data_sets(input_folders, output_files)
 
