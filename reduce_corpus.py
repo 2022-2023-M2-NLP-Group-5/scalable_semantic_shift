1912c1473c017b8388ee32b70460d1cdda70ab7b
Averie so <averieso@Averies-MacBook-Pro.local>
2023-01-27 17:39:21 +0100

    add option to keep a portion of the filtered corpus

--- reduce_corpus.py
+++ reduce_corpus.py
@@ -3,44 +3,57 @@ import argparse
 from nltk.stem.snowball import SnowballStemmer
 from nltk.stem.cistem import Cistem
 
+
 def reduce_corpus(corpus_filepath,
                   output_filepath,
-                  target_filepath,
                   lang,
-                  self_defined_words = "",
-                  semeval_eng = False):
+                  target_filepath="",
+                  self_defined_words="",
+                  stem_self_defined=False,
+                  semeval_eng=False,
+                  keep_size=1):
     corpus = open(corpus_filepath).readlines()
     if lang == "english":
         stemmer = SnowballStemmer("english")
     elif lang == "german":
         stemmer = Cistem()
 
-    if self_defined_words != "": 
-        target = self_defined_words.split(";")
+    if self_defined_words != "":
+        targets = self_defined_words.split(";")
     else:
         targets = open(target_filepath).readlines()
+
+    if (self_defined_words != "" and stem_self_defined == True) or self_defined_words == "":
         if semeval_eng == True:
-            target = set([tok if len(tok:= stemmer.stem(t.split("_")[0])) < 4 else tok[:4] for t in targets])
-            #target = set([stemmer.stem(t.split("_")[0])[:-2] for t in targets])
+            target = set([tok if len(tok := stemmer.stem(t.split("_")[0])) < 4 else tok[:4] for t in targets])
+            # target = set([stemmer.stem(t.split("_")[0])[:-2] for t in targets])
         else:
-            target = set([tok if len(tok:= stemmer.stem(t.split("\n")[0])) < 4 else tok[:4] for t in targets])
-            #target = set([stemmer.stem(t.split("\n")[0])[:-2] for t in targets])
+            target = set([tok if len(tok := stemmer.stem(t.split("\n")[0])) < 4 else tok[:4] for t in targets])
+            # target = set([stemmer.stem(t.split("\n")[0])[:-2] for t in targets])
+    else:
+        target = [w.lower() for w in targets]
 
     print("Querying these words:", target)
 
-    keep = count = 0
+    count = 0
+    all_lines = []
     with open(os.path.join(output_filepath), "w") as f:
-        for line in corpus:
-            #text = {tok if len(tok:= stemmer.stem(word)) < 4 else tok[:-1] for word in set(line.split())}
-            text = {x for x in set(line.split()) for i in target if (x.startswith(i))}
+        for e, line in enumerate(corpus):
+            # text = {tok if len(tok:= stemmer.stem(word)) < 4 else tok[:-1] for word in set(line.split())}
+            text = {x for x in set(line.split()) for i in target if (x.lower().startswith(i))}
             count += 1
-            #if target.intersection(text) == set():
             if len(text) == 0:
                 continue
             else:
-                keep += 1
-                f.write(line)
-    print("Number of lines in reduced corpus:", keep)
+                all_lines.append(line)
+        if keep_size < 1:
+            new_corpus_len = int(keep_size * len(all_lines))
+            print("Keeping", new_corpus_len, "lines out of ", len(all_lines),
+                  "lines that contain stems of the targets. ")
+        else:
+            new_corpus_len = len(all_lines)
+        f.write("\n".join(all_lines[:new_corpus_len]))
+    print("Number of lines in reduced corpus:", new_corpus_len)
     print("Number of lines in original corpus:", count)
 
 
@@ -63,6 +76,10 @@ if __name__ == '__main__':
                         default="english",
                         type=str,
                         help="english / german")
+    parser.add_argument("--stem_self_defined",
+                        default= False,
+                        type=bool,
+                        help="whether to filter with the stem the self defined word")
     parser.add_argument("--self_defined_words",
                         default="",
                         type=str,
@@ -71,6 +88,10 @@ if __name__ == '__main__':
                         default=False,
                         type=bool,
                         help="True only if the targets are from semeval english")
+    parser.add_argument("--keep_size",
+                        default=1,
+                        type=float,
+                        help="a value between 0 and 1 to denote the proportion of filtered corpus to keep")
     args = parser.parse_args()
 
     reduce_corpus(corpus_filepath = args.corpus_filepath,
@@ -78,5 +99,6 @@ if __name__ == '__main__':
                   target_filepath = args.target_filepath,
                   lang = args.lang,
                   self_defined_words = args.self_defined_words,
-                  semeval_eng = args.semeval_eng)
-
+                  stem_self_defined = args.stem_self_defined,
+                  semeval_eng = args.semeval_eng,
+                  keep_size = args.keep_size)
